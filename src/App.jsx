9858a5b521d7f518c5758257a6b16a745ab69824
lahiruthpala <lahiruthpala@gmail.com>
2024-07-02 19:57:15 +0530

    Contact details

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {VoterRegistration} from "./Pages/VoterRegistration/VoterRegistration_1";
-import {Footer} from "./Components/Footer.jsx";
+import {Header} from "./Components/Header.jsx";
+import {Navbar} from "./Components/Navbar.jsx";
+import {Breadcrumbs} from "./Components/Breadcrumbs.jsx";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                {/*<Navbar />*/}
+                <Header/>
+                <Navbar/>
+                <Breadcrumbs/>
                 <Routes>
-                    <Route path="/VoterRegistration" element={<VoterRegistration />} />
-                    <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+                    <Route path="/VoterRegistration" element={<VoterRegistration/>}/>
+                    <Route path="*" element={<h1> PAGE NOT FOUND</h1>}/>
                 </Routes>
-                <Footer />
             </Router>
         </div>
     );
