5162eccb2075db7c56c1adea57b4a7a3e30040ea
damitha sahan <sahanchandrasena462@gmail.com>
2024-07-19 15:51:08 +0530

    politician card updatetd with navigation

--- src/Components/PoliticianCard.jsx
+++ src/Components/PoliticianCard.jsx
@@ -4,10 +4,37 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import {Navigate, useNavigate} from "react-router-dom";
 
 export const Politician = () => {
+
+  const navigate = useNavigate();
+
+  // const navigator = (action) => {
+  //     if(action === "Party Details"){
+  //         navigate(`/party/list`);
+  //     }else if(action === "Candidates"){
+
+  //     }else if(action === "Elections"){
+  //         navigate(`/election/list`);
+  //     }else if(action === "Inquiries"){
+  //         navigate(`/inquiries`);
+  //     }else if(action === "Announcements"){
+  //         navigate(`/announcements`);
+  //     }else if(action === "Voter Registration"){
+  //         navigate(`/VoterRegistration`);
+  //     }else{
+  //         console.log("Invalid navigation");
+  //     }
+  //     console.log(action);
+  // };
+
+  const navigator = () => {
+    navigate('/party/member');
+  }
+
   return (
-    <Card className='w-full '>
+    <Card className='w-full ' onClick={navigator}>
       <CardActionArea>
         <div className='flex'> 
             <img
